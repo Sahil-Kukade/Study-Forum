41b3d4d7ea30a58573be44858c241e19d3fe65f7
Sahil-Kukade <sahilkukade2003@gmail.com>
2025-07-24 10:23:09 +0530

    fix: update CORS origin for vercel frontend

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());  // aapn authentication kiva authorization sathi json w
 app.use(express.urlencoded({extended:true}));  // use to parse url encodedBodies
 app.use(
 	cors({
-		origin:"http://localhost:3000",   // we will see at frontend time
+		origin:"https://study-forum.vercel.app",   // we will see at frontend time
 		credentials:true,
 
 		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Explicitly allow common methods
